1220aa1db6bdc449ce643afa815c8f3ec11269c9
KimSuHyeon <suhyeonnim0325@gmail.com>
2024-01-25 13:43:49 +0900

    Update settings.py

--- djangogmail/djangogmail/settings.py
+++ djangogmail/djangogmail/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'suhyeonnim0325@gmail.com'
-EMAIL_HOST_PASSWORD = 'vanv pzhq bkxx fwed'
+EMAIL_HOST_USER = 's'
+EMAIL_HOST_PASSWORD = 'v'
